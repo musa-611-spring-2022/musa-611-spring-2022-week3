7b12e99187100b6fc3cf7af4f2d2ca4f15761600
Jiamin Tan <tan_jiamin@yahoo.com>
2022-01-31 15:15:23 -0500

    completed part1 step2

--- exercise/part1-fizzbuzz/index.js
+++ exercise/part1-fizzbuzz/index.js
@@ -25,7 +25,13 @@ Start code
 
 ===================== */
 for (let i = 1; i < 101; i++) {
-  console.log(i);
+  if (i % 3 === 0) {
+    console.log('Fizz');
+  } else if (i % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
+  }
 }
 
 
