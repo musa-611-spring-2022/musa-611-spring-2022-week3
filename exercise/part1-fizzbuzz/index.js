687e49bda2367203939775114c35738f56ea15b2
johnatawnclementawn <johnathan.clementi1@gmail.com>
2022-01-31 16:22:00 -0500

    Lol I had Buzz for %3 not Fizz

--- exercise/part1-fizzbuzz/index.js
+++ exercise/part1-fizzbuzz/index.js
@@ -30,7 +30,7 @@ for (let i = 1; i <= 100; i++){
         console.log("FizzBuzz");
     }
     else if (i % 3 === 0) {
-        console.log("Buzz");
+        console.log("Fizz");
     }
     else if (i % 5 === 0) {
         console.log("Buzz");
