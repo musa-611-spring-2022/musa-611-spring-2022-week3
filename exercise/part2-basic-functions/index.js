f86bd39ac563a65934ed2c926d501376e6385595
Shaun.xiongzheng <shaun_zheng@163.com>
2022-01-31 17:57:12 -0500

    1

--- exercise/part2-basic-functions/index.js
+++ exercise/part2-basic-functions/index.js
@@ -10,8 +10,12 @@ Instructions: Write a function, age, that takes a birth year and returns an age
 in years. (Let's just assume this person was born January 1 at 12:01 AM)
 Example: "age(2000) should return 22"
 ===================== */
+var Year = new Date().getFullYear();
 
-let age = (birthYear) => {};
+let age = (birthYear) => {
+    outCome = Year - birthYear
+    return outCome;
+};
 
 console.log('age success:', age(1971) === 51);
 
@@ -20,7 +24,10 @@ Instructions: Write a function that adds one to the number provided
 Example: "plusOne(2) should return 3"
 ===================== */
 
-let plusOne = () => {};
+let plusOne = (num) => {
+    num ++
+    return num;
+};
 
 console.log('plusOne success:', plusOne(99) === 100);
 
@@ -29,7 +36,10 @@ Instructions: Write a function that multiplies the number provided by three
 Example: "timesThree(2) should return 6"
 ===================== */
 
-let timesThree = () => {};
+let timesThree = (num) => {
+    num = num * 3
+    return num
+};
 
 console.log('timesThree success:', timesThree(33) === 99);
 
@@ -38,7 +48,10 @@ Instructions: Write a function that adds two given numbers
 Example: "add(2, 3) should return 5"
 ===================== */
 
-let add = () => {};
+let add = (a,b) => {
+    total = a + b
+    return total;
+};
 
 console.log('add success:', add(4, 6) === 10);
 
@@ -47,7 +60,10 @@ Instructions: Write a function that multiplies two given numbers
 Example: "multiply(2, 3) should return 6"
 ===================== */
 
-let multiply = () => {};
+let multiply = (a,b) => {
+    total = a * b
+    return total
+};
 
 console.log('multiply success:', multiply(4, 6) === 24);
 
@@ -56,7 +72,10 @@ Instructions: "Write a function that returns the value of an array at a specifie
 Example: "valueAtIndex(['Mercury', 'Venus', 'Earth', 'Mars'], 2) should return 'Earth'"
 ===================== */
 
-let valueAtIndex = () => {};
+let valueAtIndex = (list, index) => {
+    outCome = list[index]
+    return outCome
+};
 
 console.log('valueAtIndex success:', valueAtIndex([9, 8, 7, 6, 5], 2) === 7);
 
@@ -65,7 +84,10 @@ Instructions: "Write a function that returns the value of an object at a specifi
 Example: "valueAtKey({'name': 'Nathan'}, 'name') should return 'Nathan'"
 ===================== */
 
-let valueAtKey = () => {};
+let valueAtKey = (objectHere, keyHere) => {
+    value = objectHere[keyHere]
+    return value
+};
 
 console.log('valueAtKey success:', valueAtKey({'foo': 'bar'}, 'foo') === 'bar');
 
@@ -73,7 +95,13 @@ console.log('valueAtKey success:', valueAtKey({'foo': 'bar'}, 'foo') === 'bar');
 Instructions: Write a function that returns true if a number is even.
 ===================== */
 
-let isEven = () => {};
+let isEven = (num) => {
+    if (num%2 == 0){
+        return true;
+    } else{
+        return false;
+    }
+};
 
 console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 
@@ -81,6 +109,12 @@ console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 Instructions: Write a function that returns true if a number is odd.
 ===================== */
 
-let isOdd = () => {};
+let isOdd = (num) => {
+    if (num%2 == 0){
+        return false;
+    } else{
+        return true;
+    }
+};
 
 console.log('isOdd success:', isOdd(4) === false && isOdd(5) === true);
--- exercise/part3-iterators/index.js
+++ exercise/part3-iterators/index.js
@@ -13,7 +13,9 @@ Instructions: Write a function which counts the number of times a value occurs i
 Example: countItem(['a', 'b', 'a'], 'a') should return 2
 ===================== */
 
-let countItem = (arr, item) => {};
+let countItem = (arr, item) => {
+  trueArr = arr.filter(single => single == item)
+  return trueArr.length;}
 
 console.log('countItem success:', countItem([1, 2, 3, 4, 5, 4, 4], 4) === 3);
 
@@ -24,8 +26,17 @@ Instructions: Write a function which takes an array and returns a new array with
   original array.
 ===================== */
 
-let isEven = (n) => {};
-let filterEven = (arr) => {};
+let isEven = (n) => {
+  if (n % 2 == 0){
+    return true;
+  } else {
+    return false;
+  }
+};
+let filterEven = (arr) => {
+  anotherArr = arr.filter(single=> isEven(single))
+  return anotherArr;
+};
 
 console.log('filterEven success:', _(filterEven([1, 2, 3, 4, 5, 4, 4])).isEqual([2, 4, 4, 4]));
 
@@ -35,8 +46,17 @@ Instructions: Write a function which takes an array and returns a new array with
   function MUST use the isOdd function and MUST NOT change the original array.
 ===================== */
 
-let isOdd = (n) => {};
-let filterOdd = (arr) => {};
+let isOdd = (n) => {
+  if (n % 2 == 0){
+    return false;
+  } else {
+    return true;
+  }
+};
+let filterOdd = (arr) => {
+  anotherArr = arr.filter(single=> isOdd(single))
+  return anotherArr;
+};
 
 console.log('filterOdd success:', _(filterOdd([1, 2, 3, 4, 5, 4, 4])).isEqual([1, 3, 5]));
 
@@ -47,8 +67,14 @@ Instructions: Write a function which takes an array and returns a new array with
   original array.
 ===================== */
 
-let plusOne = (n) => {};
-let mapIncrement = (arr) => {};
+let plusOne = (n) => {
+  n = n + 1
+  return n;
+};
+let mapIncrement = (arr) => {
+  arr = arr.map(plusOne)
+  return arr;
+};
 
 console.log('mapIncrement success:', _(mapIncrement([1, 2, 3, 4, 5, 4, 4])).isEqual([2, 3, 4, 5, 6, 5, 5]));
 
@@ -59,10 +85,16 @@ Instructions: Write a function which takes an array and returns a new array with
   original array.
 ===================== */
 
-let timesThree = (n) => {};
-let mapTriple = (arr) => {};
+let timesThree = (n) => {
+  n = n * 3
+  return n;
+};
+let mapTriple = (arr) => {
+  arr = arr.map(timesThree)
+  return arr;
+};
 
-console.log('mapTriple success:', _(mapIncrement([1, 2, 3, 4, 5, 4, 4])).isEqual([3, 6, 9, 12, 15, 12, 12]));
+console.log('mapTriple success:', _(mapTriple([1, 2, 3, 4, 5, 4, 4])).isEqual([3, 6, 9, 12, 15, 12, 12]));
 
 /* =====================
 Instructions: Write a function which takes an array and returns the sum of all
@@ -71,8 +103,13 @@ Instructions: Write a function which takes an array and returns the sum of all
   array.
 ===================== */
 
-let add = (n1, n2) => {};
-let reduceSum = (arr) => {};
+let add = (n1, n2) => {
+  return n1 + n2;
+};
+let reduceSum = (arr) => {
+  total = arr.reduce(add)
+  return total;
+};
 
 console.log('reduceSum success:', reduceSum([1, 2, 3, 4, 5, 4, 4]) === 23);
 
@@ -83,7 +120,11 @@ Instructions: Write a function which takes an array and returns the product of
   original array.
 ===================== */
 
-let multiply = (n1, n2) => {};
-let reduceProduct = (arr) => {};
+let multiply = (n1, n2) => {
+  return n1 * n2;
+};
+let reduceProduct = (arr) => {
+  return arr.reduce(multiply);
+};
 
 console.log('reduceProduct success:', reduceProduct([1, 2, 3, 4, 5, 4, 4]) === 1920);
