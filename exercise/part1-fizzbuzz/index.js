957c80b1108964c9c43dfe813ecf6d1ede760f81
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2022-01-31 16:28:22 -0500

    part 1 finish

--- exercise/part1-fizzbuzz/index.js
+++ exercise/part1-fizzbuzz/index.js
@@ -25,6 +25,25 @@ Start code
 
 ===================== */
 
+for (let i = 1; i <= 100; i += 1) {
+  if(i%3===0){
+    if(i%5===0)
+    {
+      console.log("FizzBuzz");
+    }
+    else console.log("Fizz");
+  }
+  else if(i%5===0){
+    if(i%3!==0)
+    {
+    console.log("Buzz");
+  }
+  }
+  else{
+    console.log(i);
+  }
+};
+
 
 
 
