61208529f7337ff375ccf6c4298135b9bef1b820
jyl <97498760+jyl-jyl@users.noreply.github.com>
2022-02-01 11:52:57 -0500

    Modified exercise1's style and conditions

--- exercise/part1-fizzbuzz/index.js
+++ exercise/part1-fizzbuzz/index.js
@@ -29,9 +29,15 @@ Start code
 // };
 
 for (let i = 1; i < 101; i++) {
-  if (i % 3 === 0 && i % 5 === 0) { console.log('FizzBuzz'); }
-  if (i % 3 === 0) { console.log('Fizz'); }
-  if (i % 5 === 0) { console.log('Buzz'); }
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz');
+  } else if (i % 3 === 0) {
+    console.log('Fizz');
+  } else if (i % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
+  }
 }
 
 
