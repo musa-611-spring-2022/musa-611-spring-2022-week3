6779a0d70040d4964d67a8e83c0c7296a533bfde
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2022-02-01 21:47:27 -0500

    correct part1

--- exercise/part1-fizzbuzz/index.js
+++ exercise/part1-fizzbuzz/index.js
@@ -25,23 +25,19 @@ Start code
 
 ===================== */
 
-for (let i = 1; i <= 100; i += 1) {
+for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0) {
-    if ( i % 5 === 0)
-    {
-      console.log( "FizzBuzz" );
+    if (i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else {
+      console.log('Fizz');
     }
-    else {
-      console.log( "Fizz" );
-     }
+  } else if (i % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
   }
-  else if ( i % 5 === 0 ) {
-    console.log( "Buzz" );
-  }
-  else {
-    console.log( i );
-  }
-};
+}
 
 
 
