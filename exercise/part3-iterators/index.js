1a78df64ed313e194a5ab096bbd9a77536ca3336
johnatawnclementawn <johnathan.clementi1@gmail.com>
2022-02-04 01:01:48 -0500

    Filter approach rather than long for loop with conditionals

--- exercise/part3-iterators/index.js
+++ exercise/part3-iterators/index.js
@@ -13,18 +13,8 @@ Instructions: Write a function which counts the number of times a value occurs i
 Example: countItem(['a', 'b', 'a'], 'a') should return 2
 ===================== */
 
-let countItem = (arr, item) => {};
-  let count = 0;
-  for(let i = 0; i <= arr.length; i++) {
-    if(arr[i] === item){
-      count++;
-      console.log(arr[i]);
-    }
-    else{
-      console.log(arr[i]);
-    };
-    return count;
-  };
+let countItem = (arr, item) => {
+  return arr.filter((countItem) => countItem === item).length;
 };
 
 console.log('countItem success:', countItem([1, 2, 3, 4, 5, 4, 4], 4) === 3);
@@ -36,8 +26,10 @@ Instructions: Write a function which takes an array and returns a new array with
   original array.
 ===================== */
 
-let isEven = (n) => {};
-let filterEven = (arr) => {};
+let isEven = (n) => {n % 2 === 0};
+let filterEven = (arr) => {
+  arr.filter(isEven);
+};
 
 console.log('filterEven success:', _(filterEven([1, 2, 3, 4, 5, 4, 4])).isEqual([2, 4, 4, 4]));
 
@@ -47,8 +39,10 @@ Instructions: Write a function which takes an array and returns a new array with
   function MUST use the isOdd function and MUST NOT change the original array.
 ===================== */
 
-let isOdd = (n) => {};
-let filterOdd = (arr) => {};
+let isOdd = (n) => {n % 2 === 1};
+let filterOdd = (arr) => {
+  arr.filter(isOdd);
+};
 
 console.log('filterOdd success:', _(filterOdd([1, 2, 3, 4, 5, 4, 4])).isEqual([1, 3, 5]));
 
@@ -74,7 +68,7 @@ Instructions: Write a function which takes an array and returns a new array with
 let timesThree = (n) => {};
 let mapTriple = (arr) => {};
 
-console.log('mapTriple success:', _(mapTriple([1, 2, 3, 4, 5, 4, 4])).isEqual([3, 6, 9, 12, 15, 12, 12]));
+console.log('mapTriple success:', _(mapIncrement([1, 2, 3, 4, 5, 4, 4])).isEqual([3, 6, 9, 12, 15, 12, 12]));
 
 /* =====================
 Instructions: Write a function which takes an array and returns the sum of all
