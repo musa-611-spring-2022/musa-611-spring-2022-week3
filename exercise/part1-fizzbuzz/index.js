1889e2a03191f794e9ec35c7f02331e23ca6471e
johnatawnclementawn <johnathan.clementi1@gmail.com>
2022-02-03 23:31:39 -0500

    Missed a parens

--- exercise/part1-fizzbuzz/index.js
+++ exercise/part1-fizzbuzz/index.js
@@ -31,7 +31,7 @@ for (let i = 1; i <= 100; i++){
     } else if (i % 3 === 0) {
         console.log('Fizz');
     } else if (i % 5 === 0) {
-        console.log'Buzz');
+        console.log('Buzz');
     } else {
         console.log(i);
     }
