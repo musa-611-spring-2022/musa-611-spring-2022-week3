cb420d9d276caf919a6031a08b86600a119e9f86
johnatawnclementawn <johnathan.clementi1@gmail.com>
2022-02-05 18:17:25 -0500

    Sections 1 and 2 of part 4 complete

--- exercise/part4-functions-as-values/index.js
+++ exercise/part4-functions-as-values/index.js
@@ -16,7 +16,16 @@ Instructions: Write a function which takes an array and returns a new array,
   on whether the item satisfies some condition).
 ===================== */
 
-let filter = (arr, pred) => {};
+let filter = (arr, pred) => {
+  let resultArr = [];
+  for(let i = 0; i < arr.length; i++) {
+    if (pred(arr[i])) {
+      resultArr.push(arr[i]);
+    }
+  }
+  return resultArr;
+};
+
 
 console.log('filter success #1:', _(filter([1, 2, 3, 4, 5, 4, 4], isEven)).isEqual([2, 4, 4, 4]));
 console.log('filter success #2:', _(filter([1, 2, 3, 4, 5, 4, 4], isOdd)).isEqual([1, 3, 5]));
@@ -26,7 +35,13 @@ Instructions: Write a function which takes an array and returns a new array,
   where each item has a function applied to it.
 ===================== */
 
-let map = (arr, func) => {};
+let map = (arr, func) => {
+  let resultArr = [];
+  for(let i = 0; i < arr.length; i++) {
+    resultArr.push(func(arr[i]));
+  }
+  return resultArr;
+};
 
 console.log('map success #1:', _(map([1, 2, 3, 4, 5, 4, 4], plusOne)).isEqual([2, 3, 4, 5, 6, 5, 5]));
 console.log('map success #2:', _(map([1, 2, 3, 4, 5, 4, 4], timesThree)).isEqual([3, 6, 9, 12, 15, 12, 12]));
