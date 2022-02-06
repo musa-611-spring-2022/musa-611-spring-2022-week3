4476569086590c9794cd970bed010de3ad31c012
YOUR NAME <mangia@upenn.edu>
2022-02-06 01:08:00 -0500

    Update index.js

--- exercise/part1-fizzbuzz/index.js
+++ exercise/part1-fizzbuzz/index.js
@@ -28,13 +28,13 @@ Start code
 for (let num = 1; num <= 100; num++) {
   if ((num % 3 === 0) && (num % 5 === 0)) {
     console.log('FizzBuzz');
-} else if (num % 5 === 0) {
-          console.log('Buzz');
-        } else if (num % 3 === 0){
-           console.log('Fizz');
-         } else {
-           console.log (num);
-         }
+  } else if (num % 5 === 0) {
+    console.log('Buzz');
+  } else if (num % 3 === 0) {
+    console.log('Fizz');
+  } else {
+    console.log(num);
+  }
 }
 
 
