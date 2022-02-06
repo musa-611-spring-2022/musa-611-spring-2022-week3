26cba7141bbb7b1d64a36517566e1b0f7d2175c6
YOUR NAME <mangia@upenn.edu>
2022-02-06 01:03:45 -0500

    Update index.js

--- exercise/part1-fizzbuzz/index.js
+++ exercise/part1-fizzbuzz/index.js
@@ -27,13 +27,14 @@ Start code
 
 for (let num = 1; num <= 100; num++) {
   if ((num % 3 === 0) && (num % 5 === 0)) {
-    console.log('FizzBuzz')};
-    else if (num % 5 === 0) {
-      console.log('Buzz')};
-      else if (num % 3 === 0){
-        console.log('Fizz')};
-        else {
-           console.log(num)};
+    console.log('FizzBuzz');
+} else if (num % 5 === 0) {
+          console.log('Buzz');
+        } else if (num % 3 === 0){
+           console.log('Fizz');
+         } else {
+           console.log (num);
+         }
 }
 
 
