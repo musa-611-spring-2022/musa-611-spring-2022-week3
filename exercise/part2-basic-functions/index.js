1701f2f893f868006d5caa0224bdc41645170612
keeea <lexi.xiao@outlook.com>
2022-02-06 23:23:25 -0500

    code style

--- exercise/part2-basic-functions/index.js
+++ exercise/part2-basic-functions/index.js
@@ -11,9 +11,7 @@ in years. (Let's just assume this person was born January 1 at 12:01 AM)
 Example: "age(2000) should return 22"
 ===================== */
 
-let age = (birthYear) =>{
-  return 2022 - birthYear;
-};
+let age = (birthYear) => { return 2022 - birthYear; };
 
 console.log('age success:', age(1971) === 51);
 
@@ -22,7 +20,7 @@ Instructions: Write a function that adds one to the number provided
 Example: "plusOne(2) should return 3"
 ===================== */
 
-let plusOne = (number) =>{
+let plusOne = (number) => {
   return number + 1;
 };
 
@@ -33,7 +31,7 @@ Instructions: Write a function that multiplies the number provided by three
 Example: "timesThree(2) should return 6"
 ===================== */
 
-let timesThree = (number) =>{
+let timesThree = (number) => {
   return number * 3;
 };
 
@@ -44,7 +42,7 @@ Instructions: Write a function that adds two given numbers
 Example: "add(2, 3) should return 5"
 ===================== */
 
-let add = (num1, num2) =>{
+let add = (num1, num2) => {
   return num1 + num2;
 };
 
@@ -55,7 +53,7 @@ Instructions: Write a function that multiplies two given numbers
 Example: "multiply(2, 3) should return 6"
 ===================== */
 
-let multiply = (num1, num2) =>{
+let multiply = (num1, num2) => {
   return num1 * num2;
 };
 
@@ -66,7 +64,7 @@ Instructions: "Write a function that returns the value of an array at a specifie
 Example: "valueAtIndex(['Mercury', 'Venus', 'Earth', 'Mars'], 2) should return 'Earth'"
 ===================== */
 
-let valueAtIndex = (array, index) =>{
+let valueAtIndex = (array, index) => {
   return array[index];
 };
 
@@ -77,7 +75,7 @@ Instructions: "Write a function that returns the value of an object at a specifi
 Example: "valueAtKey({'name': 'Nathan'}, 'name') should return 'Nathan'"
 ===================== */
 
-let valueAtKey = (object, key) =>{
+let valueAtKey = (object, key) => {
   return object[key];
 };
 
@@ -87,7 +85,7 @@ console.log('valueAtKey success:', valueAtKey({ 'foo': 'bar' }, 'foo') === 'bar'
 Instructions: Write a function that returns true if a number is even.
 ===================== */
 
-let isEven = (number) =>{
+let isEven = (number) => {
   return number % 2 === 0;
 };
 
@@ -97,7 +95,7 @@ console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 Instructions: Write a function that returns true if a number is odd.
 ===================== */
 
-let isOdd = (number) =>{
+let isOdd = (number) => {
   return number % 2 === 1;
 };
 
