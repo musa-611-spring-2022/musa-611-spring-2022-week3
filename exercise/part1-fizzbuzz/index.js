c8f43116f6f85d1aceece2a4a1c53365f9cb775e
capollon <capollon316@gmail.com>
2022-02-06 19:56:29 -0500

    Update Index.js
    fixed syntax

--- exercise/part1-fizzbuzz/index.js
+++ exercise/part1-fizzbuzz/index.js
@@ -25,16 +25,18 @@ Start code
 
 ===================== */
 
-for(let i = 0; i <= 99; i++){
-    if(i%3=0){
-      console.log("Fizz")};
-    if(i%5=0){
-      console.log("Buzz")};
-    if ((i%5=0) && (i%3=0)){
-      console.log("FizzBuzz")};
-    else {
+for(let i = 1; i <= 100; i++){
+    if(i % 3=== 0 && i % 5 ===0){
+      console.log('FizzBuzz');
+  } else if(i % 5 === 0){
+      console.log('Buzz');
+  } else if (i % 3 === 0){
+      console.log('Fizz');
+  } else {
       console.log(i);
-    };
+  }
+}  
+
 
 
 
