99f718a27c2bf87bf4a44669e415698eec41ab34
capollon <capollon316@gmail.com>
2022-02-06 19:29:37 -0500

    Update
    attempt to fix code

--- exercise/part1-fizzbuzz/index.js
+++ exercise/part1-fizzbuzz/index.js
@@ -26,12 +26,12 @@ Start code
 ===================== */
 
 const toOneHundred = () => {
-  for(let i = 1; i <= 100; i++){
-    if(i%3=0)
+  for(let i = 0; i <= 99; i++){
+    if(i%3==0)
       console.log("Fizz");
-    if(i%5=0)
+    if(i%5==0)
       console.log("Buzz");
-    if ((i%5=0) && (i%3=0))
+    if ((i%5==0) && (i%3==0))
       console.log("FizzBuzz");
     else {
       console.log(i);
