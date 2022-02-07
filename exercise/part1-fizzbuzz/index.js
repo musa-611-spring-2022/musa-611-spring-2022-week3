947f3e1dfce7ef5999a8f2b6461e918be35685ba
capollon <capollon316@gmail.com>
2022-02-06 19:25:26 -0500

    Update Index.js
    added steps2-3

--- exercise/part1-fizzbuzz/index.js
+++ exercise/part1-fizzbuzz/index.js
@@ -26,8 +26,17 @@ Start code
 ===================== */
 
 const toOneHundred = () => {
-  for(let i = 1; i <= 100; i++)
-      console.log();
+  for(let i = 1; i <= 100; i++){
+    if(i%3=0)
+      console.log("Fizz");
+    if(i%5=0)
+      console.log("Buzz");
+    if ((i%5=0) && (i%3=0))
+      console.log("FizzBuzz");
+    else {
+      console.log(i);
+    };
+
 
 
 /* =====================
