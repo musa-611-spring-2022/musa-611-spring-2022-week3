01156d4430508d52fc34459cb6b6c364eff50de0
Alex Nelms <nelmsal@gmail.com>
2022-02-06 23:16:23 -0500

    exercise 3 & package

--- exercise/part3-iterators/index.js
+++ exercise/part3-iterators/index.js
@@ -33,9 +33,7 @@ Instructions: Write a function which takes an array and returns a new array with
 let isEven = (num) => { return num % 2 == 0 };
 let filterEven = (arr) => { return arr.filter(isEven) };
 
-//console.log(filterEven([1, 2, 3, 4, 5, 4, 4]))
-
-console.log('filterEven success:', filterEven([1, 2, 3, 4, 5, 4, 4])).isEqual([2, 4, 4, 4]);
+console.log('filterEven success:', _(filterEven([1, 2, 3, 4, 5, 4, 4])).isEqual([2, 4, 4, 4]));
 
 /* =====================
 Instructions: Write a function which takes an array and returns a new array with
@@ -43,8 +41,8 @@ Instructions: Write a function which takes an array and returns a new array with
   function MUST use the isOdd function and MUST NOT change the original array.
 ===================== */
 
-let isOdd = (n) => {};
-let filterOdd = (arr) => {};
+let isOdd = (num) => { return num % 2 != 0 };
+let filterOdd = (arr) => { return arr.filter(isOdd) };
 
 console.log('filterOdd success:', _(filterOdd([1, 2, 3, 4, 5, 4, 4])).isEqual([1, 3, 5]));
 
@@ -55,8 +53,8 @@ Instructions: Write a function which takes an array and returns a new array with
   original array.
 ===================== */
 
-let plusOne = (n) => {};
-let mapIncrement = (arr) => {};
+let plusOne = (original_num) =>  {return original_num + 1 ; };
+let mapIncrement = (arr) => {return arr.map(x => plusOne(x));};
 
 console.log('mapIncrement success:', _(mapIncrement([1, 2, 3, 4, 5, 4, 4])).isEqual([2, 3, 4, 5, 6, 5, 5]));
 
@@ -67,8 +65,8 @@ Instructions: Write a function which takes an array and returns a new array with
   original array.
 ===================== */
 
-let timesThree = (n) => {};
-let mapTriple = (arr) => {};
+let timesThree = (original_num) => {return original_num * 3; };
+let mapTriple = (arr) => {return arr.map(x => timesThree(x));};
 
 console.log('mapTriple success:', _(mapTriple([1, 2, 3, 4, 5, 4, 4])).isEqual([3, 6, 9, 12, 15, 12, 12]));
 
@@ -79,8 +77,8 @@ Instructions: Write a function which takes an array and returns the sum of all
   array.
 ===================== */
 
-let add = (n1, n2) => {};
-let reduceSum = (arr) => {};
+let add = (first, second) => {return first + second ; };
+let reduceSum = (arr) => {return arr.reduce((a, b) => add(a,b), 0)};
 
 console.log('reduceSum success:', reduceSum([1, 2, 3, 4, 5, 4, 4]) === 23);
 
@@ -91,7 +89,7 @@ Instructions: Write a function which takes an array and returns the product of
   original array.
 ===================== */
 
-let multiply = (n1, n2) => {};
-let reduceProduct = (arr) => {};
+let multiply = (first, second) => { return first * second ; };
+let reduceProduct = (arr) => {return arr.reduce((a, b) => multiply(a,b), 1)};
 
 console.log('reduceProduct success:', reduceProduct([1, 2, 3, 4, 5, 4, 4]) === 1920);
