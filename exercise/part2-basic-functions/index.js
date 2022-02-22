f86415dde0fce3e41cc214df00a58f5eaee7c973
Yebei <yyebei@upenn.edu>
2022-02-22 00:32:55 -0500

    modification test 1

--- exercise/part2-basic-functions/index.js
+++ exercise/part2-basic-functions/index.js
@@ -11,9 +11,7 @@ in years. (Let's just assume this person was born January 1 at 12:01 AM)
 Example: "age(2000) should return 22"
 ===================== */
 
-let age = (birthYear) => {
-  return 2022 - birthYear;
-};
+let age = (birthYear) => { return 2022 - birthYear };
 
 console.log('age success:', age(1971) === 51);
 
@@ -22,9 +20,7 @@ Instructions: Write a function that adds one to the number provided
 Example: "plusOne(2) should return 3"
 ===================== */
 
-let plusOne = (i) => {
-  return i + 1;
-};
+let plusOne = (i) => { return i + 1 };
 
 console.log('plusOne success:', plusOne(99) === 100);
 
@@ -33,9 +29,7 @@ Instructions: Write a function that multiplies the number provided by three
 Example: "timesThree(2) should return 6"
 ===================== */
 
-let timesThree = (a) => {
-  return a * 3;
-};
+let timesThree = (a) => { return a * 3 };
 
 console.log('timesThree success:', timesThree(33) === 99);
 
@@ -44,9 +38,7 @@ Instructions: Write a function that adds two given numbers
 Example: "add(2, 3) should return 5"
 ===================== */
 
-let add = (a,b) => {
-  return a + b;
-};
+let add = (a,b) => { return a + b };
 
 console.log('add success:', add(4, 6) === 10);
 
@@ -55,9 +47,7 @@ Instructions: Write a function that multiplies two given numbers
 Example: "multiply(2, 3) should return 6"
 ===================== */
 
-let multiply = (a,b) => {
-  return a * b;
-};
+let multiply = (a,b) => { return a * b };
 
 console.log('multiply success:', multiply(4, 6) === 24);
 
@@ -66,9 +56,7 @@ Instructions: "Write a function that returns the value of an array at a specifie
 Example: "valueAtIndex(['Mercury', 'Venus', 'Earth', 'Mars'], 2) should return 'Earth'"
 ===================== */
 
-let valueAtIndex = (arr) => {
-  return arr[2];
-};
+let valueAtIndex = (arr,index) => { return arr[index] };
 
 console.log('valueAtIndex success:', valueAtIndex([9, 8, 7, 6, 5], 2) === 7);
 
@@ -77,9 +65,7 @@ Instructions: "Write a function that returns the value of an object at a specifi
 Example: "valueAtKey({'name': 'Nathan'}, 'name') should return 'Nathan'"
 ===================== */
 
-let valueAtKey = (obj,keyName) => {
-  return obj[keyName];
-};
+let valueAtKey = (obj,keyName) => { return obj[keyName] };
 
 console.log('valueAtKey success:', valueAtKey({'foo': 'bar'}, 'foo') === 'bar');
 
