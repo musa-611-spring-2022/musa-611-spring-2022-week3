c78cb5e38387637aee1f5bf57cff6edbb8fe05a2
Yebei <yyebei@upenn.edu>
2022-03-14 14:10:59 -0400

    maybe last

--- exercise/part2-basic-functions/index.js
+++ exercise/part2-basic-functions/index.js
@@ -11,7 +11,9 @@ in years. (Let's just assume this person was born January 1 at 12:01 AM)
 Example: "age(2000) should return 22"
 ===================== */
 
-let age = (birthYear) => {};
+let age = (birthYear) => {
+    return 2022 - birthYear;
+};
 
 console.log('age success:', age(1971) === 51);
 
@@ -20,7 +22,9 @@ Instructions: Write a function that adds one to the number provided
 Example: "plusOne(2) should return 3"
 ===================== */
 
-let plusOne = () => {};
+let plusOne = (x) => {
+  return x + 1;
+};
 
 console.log('plusOne success:', plusOne(99) === 100);
 
@@ -29,7 +33,9 @@ Instructions: Write a function that multiplies the number provided by three
 Example: "timesThree(2) should return 6"
 ===================== */
 
-let timesThree = () => {};
+let timesThree = (x) => {
+  return x * 3
+};
 
 console.log('timesThree success:', timesThree(33) === 99);
 
@@ -38,7 +44,9 @@ Instructions: Write a function that adds two given numbers
 Example: "add(2, 3) should return 5"
 ===================== */
 
-let add = () => {};
+let add = (x,y) => {
+  return x + y
+};
 
 console.log('add success:', add(4, 6) === 10);
 
@@ -47,7 +55,9 @@ Instructions: Write a function that multiplies two given numbers
 Example: "multiply(2, 3) should return 6"
 ===================== */
 
-let multiply = () => {};
+let multiply = (a,b) => {
+  return a * b
+};
 
 console.log('multiply success:', multiply(4, 6) === 24);
 
@@ -56,7 +66,9 @@ Instructions: "Write a function that returns the value of an array at a specifie
 Example: "valueAtIndex(['Mercury', 'Venus', 'Earth', 'Mars'], 2) should return 'Earth'"
 ===================== */
 
-let valueAtIndex = () => {};
+let valueAtIndex = (array,b) => {
+  return array[b]
+};
 
 console.log('valueAtIndex success:', valueAtIndex([9, 8, 7, 6, 5], 2) === 7);
 
