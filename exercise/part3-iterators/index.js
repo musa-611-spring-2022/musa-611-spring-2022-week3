f21f14edf75afee139ab3c343f68692ca7de9b3d
Yebei <90070226+yyebei@users.noreply.github.com>
2022-03-15 01:50:18 -0400

    0315-3

--- exercise/part3-iterators/index.js
+++ exercise/part3-iterators/index.js
@@ -15,7 +15,7 @@ in an array
 Example: countItem(['a', 'b', 'a'], 'a') should return 2
 ===================== */
 
-let countItem = (arr, item) => arr.reduce((a, v) => (v === item ? a += 1 : a), 0);
+let countItem = (arr, item) => arr.reduce((a, v) => (v === item ? a + 1 : a), 0);
 console.log('countItem success:', countItem([1, 2, 3, 4, 5, 4, 4], 4) === 3);
 
 /* =====================
@@ -37,7 +37,7 @@ Instructions: Write a function which takes an array and returns a new array with
 ===================== */
 
 let isOdd = (a) => a % 2 !== 0;
-let filterOdd = (arr) => arr.filter((ele) => isOdd(ele))
+let filterOdd = (arr) => arr.filter((ele) => isOdd(ele));
 
 console.log('filterOdd success:', _(filterOdd([1, 2, 3, 4, 5, 4, 4])).isEqual([1, 3, 5]));
 
@@ -49,7 +49,7 @@ Instructions: Write a function which takes an array and returns a new array with
 ===================== */
 
 let plusOne = (n) => n + 1;
-let mapIncrement = (arr) => arr.map((ele) => plusOne(ele))
+let mapIncrement = (arr) => arr.map((ele) => plusOne(ele));
 
 console.log('mapIncrement success:', _(mapIncrement([1, 2, 3, 4, 5, 4, 4])).isEqual([2, 3, 4, 5, 6, 5, 5]));
 
@@ -61,7 +61,7 @@ Instructions: Write a function which takes an array and returns a new array with
 ===================== */
 
 let timesThree = (n) => n * 3;
-let mapTriple = (arr) => arr.map((ele) => timesThree(ele))
+let mapTriple = (arr) => arr.map((ele) => timesThree(ele));
 
 console.log('mapTriple success:', _(mapTriple([1, 2, 3, 4, 5, 4, 4])).isEqual([3, 6, 9, 12, 15, 12, 12]));
 
