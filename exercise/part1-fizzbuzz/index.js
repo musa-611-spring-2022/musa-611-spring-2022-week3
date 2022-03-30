ba7488cd0a34335aad54393f47f0dd40b2cdf6e7
Xiaoyi <xiaoyi_wu@163.com>
2022-03-30 10:27:30 -0400

    update

--- exercise/part1-fizzbuzz/index.js
+++ exercise/part1-fizzbuzz/index.js
@@ -25,8 +25,8 @@ Start code
 
 ===================== */
 
-for (let n = 1; n < 101; n += 1) {
-  if (n % 3 === 0 && n % 5 === 0) {
+for (let n = 1; n <= 100; n += 1) {
+  if (n % 5 === 0 && n % 3 === 0) {
     console.log('FizzBuzz');
   } else if (n % 3 === 0) {
     console.log('Fizz');
--- exercise/part2-basic-functions/index.js
+++ exercise/part2-basic-functions/index.js
@@ -11,7 +11,7 @@ in years. (Let's just assume this person was born January 1 at 12:01 AM)
 Example: "age(2000) should return 22"
 ===================== */
 
-const age = (birthYear) => 2022 - birthYear;
+let age = (birthYear) => 2022 - birthYear;
 
 console.log('age success:', age(1971) === 51);
 
@@ -20,7 +20,7 @@ Instructions: Write a function that adds one to the number provided
 Example: "plusOne(2) should return 3"
 ===================== */
 
-const plusOne = (num) => num + 1;
+let plusOne = (num) => num + 1;
 
 console.log('plusOne success:', plusOne(99) === 100);
 
@@ -29,7 +29,7 @@ Instructions: Write a function that multiplies the number provided by three
 Example: "timesThree(2) should return 6"
 ===================== */
 
-const timesThree = (x) => x * 3;
+let timesThree = (x) => x * 3;
 
 console.log('timesThree success:', timesThree(33) === 99);
 
@@ -38,7 +38,7 @@ Instructions: Write a function that adds two given numbers
 Example: "add(2, 3) should return 5"
 ===================== */
 
-const add = (a, b) => a + b;
+let add = (a, b) => a + b;
 
 console.log('add success:', add(4, 6) === 10);
 
@@ -47,7 +47,7 @@ Instructions: Write a function that multiplies two given numbers
 Example: "multiply(2, 3) should return 6"
 ===================== */
 
-const multiply = (a, b) => a * b;
+let multiply = (a, b) => a * b;
 
 console.log('multiply success:', multiply(4, 6) === 24);
 
@@ -56,7 +56,7 @@ Instructions: "Write a function that returns the value of an array at a specifie
 Example: "valueAtIndex(['Mercury', 'Venus', 'Earth', 'Mars'], 2) should return 'Earth'"
 ===================== */
 
-const valueAtIndex = (l, i) => l[i];
+let valueAtIndex = (l, i) => l[i];
 
 console.log('valueAtIndex success:', valueAtIndex([9, 8, 7, 6, 5], 2) === 7);
 
@@ -65,7 +65,7 @@ Instructions: "Write a function that returns the value of an object at a specifi
 Example: "valueAtKey({'name': 'Nathan'}, 'name') should return 'Nathan'"
 ===================== */
 
-const valueAtKey = (d, i) => d[i];
+let valueAtKey = (d, i) => d[i];
 
 console.log('valueAtKey success:', valueAtKey({ foo: 'bar' }, 'foo') === 'bar');
 
@@ -73,7 +73,7 @@ console.log('valueAtKey success:', valueAtKey({ foo: 'bar' }, 'foo') === 'bar');
 Instructions: Write a function that returns true if a number is even.
 ===================== */
 
-const isEven = (num) => num % 2 === 0;
+let isEven = (num) => num % 2 === 0;
 
 console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 
@@ -81,6 +81,6 @@ console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 Instructions: Write a function that returns true if a number is odd.
 ===================== */
 
-const isOdd = (num) => num % 2 === 1;
+let isOdd = (num) => num % 2 === 1;
 
 console.log('isOdd success:', isOdd(4) === false && isOdd(5) === true);
